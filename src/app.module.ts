f95a00f7d5bb7dad254524fbb4a8601d0dc693d1
jrodrigojrs <jrod_br@outlook.com>
2026-04-03 21:14:02 -0300

    refact: expire in from .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
+import jwtConfig from './config/jwt/jwt.config.js';
 import { PrismaModule } from './prisma/prisma.module.js';
 import { UsersModule } from './users/users.module.js';
 import { AuthModule } from './auth/auth.module.js';
 
+const configOptions: ConfigModuleOptions = {
+  isGlobal: true,
+  load: [jwtConfig as any],
+};
+
 @Module({
   imports: [
+    ConfigModule.forRoot(configOptions),
     PrismaModule, // 🔥 aqui
     UsersModule,
     AuthModule,
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,18 +5,26 @@ import { AuthController } from './auth.controller.js';
 import { UsersModule } from '../users/users.module.js';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthGuard } from '../auth/guards/auth.guard.js';
-import { JwtStrategy } from './jwt/jwt.strategy.js';
+import { ConfigService } from '@nestjs/config';
+import { StringValue } from 'ms';
 
 @Module({
   imports: [
     forwardRef(() => UsersModule),
-    JwtModule.register({
-      secret: process.env.JWT_SECRET || 'default_secret', // Use a secret from environment variables
-      signOptions: { expiresIn: '1d' }, // expiração do token
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('jwt.secret') || 'secret_default',
+        signOptions: {
+          expiresIn:
+            (configService.get<string>('jwt.expiresIn') as StringValue) ||
+            '30s',
+        },
+      }),
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, AuthGuard, JwtStrategy],
+  providers: [AuthService, AuthGuard],
   exports: [AuthGuard, AuthService, JwtModule], // Exportando AuthService e JwtModule para serem usados em outros módulos
 })
 export class AuthModule {}
